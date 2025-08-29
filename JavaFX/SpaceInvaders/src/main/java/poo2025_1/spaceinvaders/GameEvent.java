10db6cafb7aa32d2734593fa0307aff319d96c18
José Marcos Prado <jm.prado.dev@gmail.com>
2025-08-28 23:40:58 -0300

    feat: classe GameEvent para lidar com eventos disparados pelo jogo como GameOver

---
+++ JavaFX/SpaceInvaders/src/main/java/poo2025_1/spaceinvaders/GameEvent.java
@@ -0,0 +1,13 @@
+package poo2025_1.spaceinvaders;
+
+import javafx.event.Event;
+import javafx.event.EventType;
+
+public class GameEvent extends Event {
+
+    public static final EventType<GameEvent> GAME_OVER = new EventType<>(Event.ANY, "GAME_OVER");
+
+    public GameEvent(EventType<? extends Event> eventType) {
+        super(eventType);
+    }
+}
